4e8ff297e0fc24f6c5515a84d97da3a58ce3de5b
HappyFork <pickles713@hotmail.com>
2024-12-15 21:43:12 -0800

    Snowball throwing

--- nodes/player.gd
+++ nodes/player.gd
@@ -3,11 +3,21 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const TURN_DAMP = 250.0
+
+
+var snowball = preload("res://nodes/snowball.tscn")
+var sball_inst
 var mouse_motion : Vector2
 var shoot_energy : float
+
+@export var TILT_LOWER_LIMIT := -PI/2
+@export var TILT_UPPER_LIMIT := PI/2
+@export var TURN_DAMP = 250.0
+@export var POWER_GROWTH = 20.0
+
 @onready var camera = $Camera3D
-var snowball = preload("res://nodes/snowball.tscn")
+@onready var facing = $Camera3D/Marker3D
+
 
 
 func _ready():
@@ -21,6 +31,9 @@ func _unhandled_input(event):
 	# to reference input events
 	if event is InputEventMouseMotion:
 		mouse_motion += event.relative
+	
+	if event.is_action_pressed( "pause" ):
+		get_tree().quit()
 
 
 func _physics_process(delta):
@@ -33,10 +46,10 @@ func _physics_process(delta):
 	# Mouse tilt camera
 	camera.rotate_x( -mouse_motion.y / TURN_DAMP )
 	# Limit camera tilt
-	if camera.rotation.x > 1.0:
-		camera.rotation.x = 1.0
-	if camera.rotation.x < -1.0:
-		camera.rotation.x = -1.0
+	if camera.rotation.x > TILT_UPPER_LIMIT:
+		camera.rotation.x = TILT_UPPER_LIMIT
+	if camera.rotation.x < TILT_LOWER_LIMIT:
+		camera.rotation.x = TILT_LOWER_LIMIT
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -48,7 +61,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -61,24 +74,20 @@ func _physics_process(delta):
 	
 	# Handle shooting
 	if Input.is_action_just_released("shoot"):
-		# Fire using built-up energy
-		var shootforce = -transform.basis.z # Calculate facing angle
-		shootforce.rotated( Vector3.RIGHT, camera.rotation.x ) # Add camera angle
-		shootforce = shootforce.normalized() # Normalize
-		shootforce *= shoot_energy # Multiply by energy
-		# TODO: make sure this angle is figured correctly
-		throw_snowball( shootforce )
+		throw_snowball() # Fire using built-up energy
 		shoot_energy = 0.0 # Reset shoot energy
 	elif Input.is_action_pressed( "shoot" ):
 		# Increase by an arbitrary amount. I'll tweak this
 		# before making it a constant
-		shoot_energy += ( 100.0 * delta )
+		shoot_energy += ( POWER_GROWTH * delta )
 	
 	# Reset rotation
 	mouse_motion = Vector2.ZERO
 
 
-func throw_snowball( initf: Vector3 ):
-	var s : RigidBody3D = snowball.instantiate()
-	s.apply_central_impulse( initf )
-	add_child( s )
+func throw_snowball():
+	sball_inst = snowball.instantiate()
+	sball_inst.position = facing.get_global_position()
+	var force : Vector3 = facing.get_global_position() - global_position
+	sball_inst.apply_central_impulse( force * shoot_energy )
+	get_parent().add_child( sball_inst )
